7a142788b0d980d76aed4cfe08d4be96ecfd44f7
nagisa <nagisa@nagisa-air>
2026-02-05 14:32:15 +0900

    5日

--- src/main/java/com/example/samuraitrabel/SamuraitrabelApplication.java
+++ src/main/java/com/example/samuraitrabel/SamuraitrabelApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SamuraitravelApplication {
+public class SamuraitrabelApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SamuraitravelApplication.class, args);
+		SpringApplication.run(SamuraitrabelApplication.class, args);
 	}
 
 }
